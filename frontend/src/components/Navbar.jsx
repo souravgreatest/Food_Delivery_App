e03c1380448e3fc27569adc958721238ef247891
souravgreatest <jainsourav220@gmail.com>
2025-05-27 16:09:50 +0530

    UI fixing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
   const handleLogout = () => {
     localStorage.removeItem("token");
-    navigate("/login");
+    navigate("/");
   };
 
   const loadCart = () => {
--- frontend/src/screens/DashBoard.jsx
+++ frontend/src/screens/DashBoard.jsx
@@ -22,6 +22,7 @@ export default function HomePage() {
       <section
         className="d-flex align-items-center justify-content-center text-center text-white py-5"
         style={{
+            paddingLeft:"100px",
           minHeight: 'calc(100vh - 120px)', // Adjust based on navbar/footer height to fill viewport
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('https://images.unsplash.com/photo-1570560258879-af7f8e1447ac?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fHJlc3RhdXJhbnR8ZW58MHx8MHx8fDA%3D')`, // Dynamic Indian food/restaurant background
         //   backgroundSize: 'cover',
@@ -69,7 +70,7 @@ export default function HomePage() {
                 className="img-fluid"
                 style={{
                 //   maxWidth: '100%', // Adjust size
-                  marginLeft:"150px",
+                //   marginLeft:"150px",
                   height: '500px',
                   filter: 'drop-shadow(0 5px 15px rgba(0,0,0,0.6))', // Add a shadow to make it pop
                 }}
